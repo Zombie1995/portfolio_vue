0ef1dc043958bc5c7027120cb7f31cb309017405
Zombie1995 <egorkerber1995@mail.ru>
2024-02-17 12:36:24 +0300

    docker added

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,11 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx(), tsconfigPaths()],
+  server: {
+    host: true,
+    port: 8000,
+    watch: {
+      usePolling: true,
+    },
+  },
 });
