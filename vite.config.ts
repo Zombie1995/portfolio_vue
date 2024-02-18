a1be3db5f9a936cb0fee13b893b3b67748096ab6
Zombie1995 <egorkerber1995@mail.ru>
2024-02-18 15:25:40 +0300

    nginx in docker + port 80

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [vue(), vueJsx(), tsconfigPaths()],
   server: {
     host: true,
-    port: 8000,
+    port: 80,
     watch: {
       usePolling: true,
     },
